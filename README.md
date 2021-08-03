4251f3dad256758510253886f0bd24bf80f6ddd1
邹静磊 <zoujl@shinemo.com>
2021-08-03 17:15:14 +0800

    hello world opengl

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # openglesLearn
 opengles learn
+## 初始环境搭建
+ 1.brew install glfw
+ 2.https://glad.dav1d.de/ 下载glad,解压放入项目
+ 3.CMakeTool使用
+   CMake:Quick Start
+   CMake:Build  (生成程序)
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mac/Downloads/git/openglesLearn/openglesLearn
+# Build directory: /Users/mac/Downloads/git/openglesLearn/openglesLearn/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
