3903cca0ddef733bfac69dcced68a24f9efcf667
zouzouzou11 <1096174991@qq.com>
2022-08-19 10:15:00 +0800

    feat: offscreen support

---
+++ Shader.h
@@ -0,0 +1,59 @@
+#ifndef SHADER_H
+#define SHADER_H
+#include <GL/glew.h>
+
+class Shader
+{
+public:
+    unsigned int ID;
+    // constructor generates the shader on the fly
+    // ------------------------------------------------------------------------
+    Shader(const char* vShaderCode, const char* fShaderCode)
+    {
+        // 2. compile shaders
+        unsigned int vertex, fragment;
+        // vertex shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        // checkCompileErrors(vertex, "VERTEX");
+        // fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        // checkCompileErrors(fragment, "FRAGMENT");
+        // shader Program
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        // checkCompileErrors(ID, "PROGRAM");
+        // delete the shaders as they're linked into our program now and no longer necessery
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+
+    }
+    // activate the shader
+    // ------------------------------------------------------------------------
+    void use() const
+    { 
+        glUseProgram(ID); 
+    }
+    // utility uniform functions
+    // ------------------------------------------------------------------------
+    void setBool(const std::string &name, bool value) const
+    {         
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
+    }
+    // ------------------------------------------------------------------------
+    void setInt(const std::string &name, int value) const
+    { 
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
+    }
+    // ------------------------------------------------------------------------
+    void setFloat(const std::string &name, float value) const
+    { 
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+    }
+};
+#endif
--- main.cpp
+++ main.cpp
@@ -8,12 +8,14 @@
 #include <stdlib.h>
 #include <iostream>
 
+#include "Shader.h"
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+int SCR_WIDTH = 800;
+int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
@@ -27,6 +29,31 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                    "{\n"
                                    "   FragColor = vec4(0.5f, 0.8f, 0.2f, 1.0f);\n"
                                    "}\n\0";
+
+const char *vsFrameBufferScreenSource = "#version 330 core\n"
+                                        "layout (location = 0) in vec2 aPos;\n"
+                                        "layout (location = 1) in vec2 aTexCoords;\n"
+
+                                        "out vec2 TexCoords;\n"
+
+                                        "void main()\n"
+                                        "{\n"
+                                        "    TexCoords = aTexCoords;\n"
+                                        "   gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0); \n"
+                                        "}\n\0";
+
+const char *fsFrameBufferScreenSource = "#version 330 core\n"
+                                        "out vec4 FragColor;\n"
+
+                                        "in vec2 TexCoords;\n"
+
+                                        "uniform sampler2D screenTexture;\n"
+
+                                        "void main()\n"
+                                        "{\n"
+                                        "   vec3 col = texture(screenTexture, TexCoords).rgb;\n"
+                                        "   FragColor = vec4(col, 1.0);\n"
+                                        "}\n\0";
 
 int main()
 {
@@ -50,6 +77,10 @@ int main()
         glfwTerminate();
         return -1;
     }
+    //获取真实设备
+    glfwGetFramebufferSize(window, &SCR_WIDTH, &SCR_HEIGHT);
+    std::cout<<"width::"<<SCR_WIDTH<<std::endl;
+    std::cout<<"height::"<<SCR_HEIGHT<<std::endl;
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -60,7 +91,7 @@ int main()
     //     std::cout << "Failed to initialize GLAD" << std::endl;
     //     return -1;
     // }
-    //glew load
+    // glew load
     glewExperimental = GL_TRUE;
     glewInit();
 
@@ -128,6 +159,33 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
+    // uncomment this call to draw in wireframe polygons.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    //离屏绘制
+    const bool isOffScreenDraw = true;
+
+    unsigned int quadVAO_3d, quadVBO_3d;
+    float quadVertices[] = {// vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+                            // positions   // texCoords
+                            -1.0f, 1.0f, 0.0f, 1.0f,
+                            -1.0f, -1.0f, 0.0f, 0.0f,
+                            1.0f, -1.0f, 1.0f, 0.0f,
+
+                            -1.0f, 1.0f, 0.0f, 1.0f,
+                            1.0f, -1.0f, 1.0f, 0.0f,
+                            1.0f, 1.0f, 1.0f, 1.0f};
+    // screen quad VAO
+    glGenVertexArrays(1, &quadVAO_3d);
+    glGenBuffers(1, &quadVBO_3d);
+    glBindVertexArray(quadVAO_3d);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO_3d);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
+
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -135,27 +193,73 @@ int main()
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
-    // uncomment this call to draw in wireframe polygons.
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //离屏shader
+    Shader screenShader(vsFrameBufferScreenSource, fsFrameBufferScreenSource);
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
+    //帧缓冲创建
+    unsigned int framebuffer_3d;
+    unsigned int textureColorbuffer_3d;
+    glGenFramebuffers(1, &framebuffer_3d);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_3d);
+    // create a color attachment texture
+    glGenTextures(1, &textureColorbuffer_3d);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer_3d);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer_3d, 0);
+    // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);           // use a single renderbuffer object for both a depth AND stencil buffer.
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
 
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
+
         // input
         // -----
         processInput(window);
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // draw our first triangle
+        if (isOffScreenDraw)
+        {
+            glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_3d);
+            glClearColor(1.0f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
+        else
+        {
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            // draw our first triangle
+        }
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time
+
+        //离屏
+        if (isOffScreenDraw)
+        {
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+            glDisable(GL_DEPTH_TEST);
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessary actually, since we won't be able to see behind the quad anyways)
+            glClear(GL_COLOR_BUFFER_BIT);
+            screenShader.use();
+            glBindVertexArray(quadVAO_3d);
+            glBindTexture(GL_TEXTURE_2D, textureColorbuffer_3d); // use the color attachment texture as the texture of the quad plane
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
